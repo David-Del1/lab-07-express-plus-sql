333b45971d0528af43682a9188820af613988507
David Delgadillo <daviddel.731@gmail.com>
2021-05-05 08:26:05 -0700

    added pic URLs

--- data/books.js
+++ data/books.js
@@ -4,78 +4,78 @@ const books = [
     title: 'The Beginning of Infinity',
     author: 'David Deutsch',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/felix.png',
+    image_url: 'assets/BoI.jpg',
     pub_year: 2011
   },
   {
     title: 'The Fabric of Reality',
-    author: 'Orange Tabby',
+    author: 'David Deutsch',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/garfield.jpeg',
+    image_url: 'assets/FoR.jpg',
     pub_year: 1998
   },
   {
     title: 'Conjectures & Refutations',
-    author: 'Angora',
+    author: 'Karl Popper',
     genre: 'Philosophy',
-    image_url: 'cats/duchess.jpeg',
+    image_url: 'assets/CaR.jpg',
     pub_year: 1963
   },
   {
     title: 'I Am A Strange Loop',
-    author: 'Manx',
+    author: 'Douglas Hofstadter',
     genre: 'Philosophy',
-    image_url: 'cats/stimpy.jpeg',
+    image_url: 'assets/IAASL.jpg',
     pub_year: 2007
   },
   {
     title: 'The Ascent of Man',
-    author: 'Tuxedo',
+    author: 'Jacob Bronowski',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/sylvester.jpeg',
-    pub_year: 1945
+    image_url: 'assets/AoM.jpg',
+    pub_year: 1973
   },
   {
     title: 'Enlightenment Now',
-    author: 'Orange Tabby',
+    author: 'Steven Pinker',
     genre: 'Social Science/Nonficiton',
-    image_url: 'cats/tigger.jpeg',
-    pub_year: 1928
+    image_url: 'assets/EN.webp',
+    pub_year: 2018
   },
   {
     title: 'The Selfish Gene',
-    author: 'Angora',
+    author: 'Richard Dawkins',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/hello-kitty.jpeg',
-    pub_year: 1974
+    image_url: 'assets/TSG.jpg',
+    pub_year: 1976
   },
   {
     title: 'The Blind Watchmaker',
-    author: 'Angora',
+    author: 'Richard Dawkins',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/hello-kitty.jpeg',
-    pub_year: 1974
+    image_url: 'assets/TBW.jpg',
+    pub_year: 1986
   },
   {
     title: 'The Science of Can and Can\'t',
-    author: 'Orange Tabby',
+    author: 'Chiarla Marletto',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/hobbs.jpeg',
-    pub_year: 1985
+    image_url: 'assets/SoCaC.jpg',
+    pub_year: 2021
   },
   {
     title: 'Scale',
-    author: 'Orange Tabby',
+    author: 'Geoffrey West',
     genre: 'Science/Nonfiction',
-    image_url: 'cats/hobbs.jpeg',
-    pub_year: 1985
+    image_url: 'assets/scale.jpg',
+    pub_year: 2017
   },
   {
     title: 'Homo Deus',
-    author: 'Orange Tabby',
+    author: 'Yuval Noah Harrari',
     genre: 'Social Science',
-    image_url: 'cats/hobbs.jpeg',
-    pub_year: 1985
+    image_url: 'assets/HD.jpg',
+    pub_year: 2015
   }
 ];
 
