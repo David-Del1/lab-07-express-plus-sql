2e815e13c27015626b3851dc53f3a6a7f0159e15
David Delgadillo <daviddel.731@gmail.com>
2021-05-06 15:27:18 -0700

    fixed errors. all tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -86,113 +86,131 @@ describe('API Routes', () => {
       expect(response.body).toEqual(expect.arrayContaining([book1, book2, book3]));
     });
 
+    it('GET book from /api/books/:id', async () => {
+      const response = await request.get(`/api/books/${book2.id}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(book2);
+    });
+
+    it('DELETE book2 from /api/books/:id', async () => {
+      const response = await request.delete(`/api/books/${book2.id}`);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(book2);
+
+      const getResponse = await request.get('/api/books');
+
+      expect(getResponse.status).toBe(200);
+      expect(getResponse.body).toEqual(expect.arrayContaining([book1, book3]));
+    });
+
   });
 
   //   describe.skip('seed data tests', () => {
 
   //   });
 
-  const expectedBooks = [
-    {
-      title: 'The Beginning of Infinity',
-      author: 'David Deutsch',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/BoI.jpg',
-      pub_year: 2011
-    },
-    {
-      title: 'The Fabric of Reality',
-      author: 'David Deutsch',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/FoR.jpg',
-      pub_year: 1998
-    },
-    {
-      title: 'Conjectures & Refutations',
-      author: 'Karl Popper',
-      genre: 'Philosophy',
-      image_url: 'assets/CaR.jpg',
-      pub_year: 1963
-    },
-    {
-      title: 'I Am A Strange Loop',
-      author: 'Douglas Hofstadter',
-      genre: 'Philosophy',
-      image_url: 'assets/IAASL.jpg',
-      pub_year: 2007
-    },
-    {
-      title: 'The Ascent of Man',
-      author: 'Jacob Bronowski',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/AoM.jpg',
-      pub_year: 1973
-    },
-    {
-      title: 'Enlightenment Now',
-      author: 'Steven Pinker',
-      genre: 'Social Science/Nonficiton',
-      image_url: 'assets/EN.webp',
-      pub_year: 2018
-    },
-    {
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/TSG.jpg',
-      pub_year: 1976
-    },
-    {
-      title: 'The Blind Watchmaker',
-      author: 'Richard Dawkins',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/TBW.jpg',
-      pub_year: 1986
-    },
-    {
-      title: 'The Science of Can and Can\'t',
-      author: 'Chiarla Marletto',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/SoCaC.jpg',
-      pub_year: 2021
-    },
-    {
-      title: 'Scale',
-      author: 'Geoffrey West',
-      genre: 'Science/Nonfiction',
-      image_url: 'assets/scale.jpg',
-      pub_year: 2017
-    },
-    {
-      title: 'Homo Deus',
-      author: 'Yuval Noah Harrari',
-      genre: 'Social Science',
-      image_url: 'assets/HD.jpg',
-      pub_year: 2015
-    }
-  ];
+  // const expectedBooks = [
+  //   {
+  //     title: 'The Beginning of Infinity',
+  //     author: 'David Deutsch',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/BoI.jpg',
+  //     pub_year: 2011
+  //   },
+  //   {
+  //     title: 'The Fabric of Reality',
+  //     author: 'David Deutsch',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/FoR.jpg',
+  //     pub_year: 1998
+  //   },
+  //   {
+  //     title: 'Conjectures & Refutations',
+  //     author: 'Karl Popper',
+  //     genre: 'Philosophy',
+  //     image_url: 'assets/CaR.jpg',
+  //     pub_year: 1963
+  //   },
+  //   {
+  //     title: 'I Am A Strange Loop',
+  //     author: 'Douglas Hofstadter',
+  //     genre: 'Philosophy',
+  //     image_url: 'assets/IAASL.jpg',
+  //     pub_year: 2007
+  //   },
+  //   {
+  //     title: 'The Ascent of Man',
+  //     author: 'Jacob Bronowski',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/AoM.jpg',
+  //     pub_year: 1973
+  //   },
+  //   {
+  //     title: 'Enlightenment Now',
+  //     author: 'Steven Pinker',
+  //     genre: 'Social Science/Nonficiton',
+  //     image_url: 'assets/EN.webp',
+  //     pub_year: 2018
+  //   },
+  //   {
+  //     title: 'The Selfish Gene',
+  //     author: 'Richard Dawkins',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/TSG.jpg',
+  //     pub_year: 1976
+  //   },
+  //   {
+  //     title: 'The Blind Watchmaker',
+  //     author: 'Richard Dawkins',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/TBW.jpg',
+  //     pub_year: 1986
+  //   },
+  //   {
+  //     title: 'The Science of Can and Can\'t',
+  //     author: 'Chiarla Marletto',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/SoCaC.jpg',
+  //     pub_year: 2021
+  //   },
+  //   {
+  //     title: 'Scale',
+  //     author: 'Geoffrey West',
+  //     genre: 'Science/Nonfiction',
+  //     image_url: 'assets/scale.jpg',
+  //     pub_year: 2017
+  //   },
+  //   {
+  //     title: 'Homo Deus',
+  //     author: 'Yuval Noah Harrari',
+  //     genre: 'Social Science',
+  //     image_url: 'assets/HD.jpg',
+  //     pub_year: 2015
+  //   }
+  // ];
 
-  //   // If a GET request is made to /api/cats, does:
-  //   // 1) the server respond with status of 200
-  //   // 2) the body match the expected API data?
-  //   it.skip('GET /api/books', async () => {
-  //     // act - make the request
-  //     const response = await request.get('/api/books');
+  // // If a GET request is made to /api/cats, does:
+  // // 1) the server respond with status of 200
+  // // 2) the body match the expected API data?
+  // it.skip('GET /api/books', async () => {
+  //   // act - make the request
+  //   const response = await request.get('/api/books');
 
-  //     // was response OK (200)?
-  //     expect(response.status).toBe(200);
+  //   // was response OK (200)?
+  //   expect(response.status).toBe(200);
 
-  //     // did it return the data we expected?
-  //     expect(response.body).toEqual(expectedBooks);
+  //   // did it return the data we expected?
+  //   expect(response.body).toEqual(expectedBooks);
 
-  //   });
+  // });
 
-//   // If a GET request is made to /api/cats/:id, does:
-//   // 1) the server respond with status of 200
-//   // 2) the body match the expected API data for the cat with that id?
-//   test('GET /api/books/:id', async () => {
-//     const response = await request.get('/api/books/2');
-//     expect(response.status).toBe(200);
-//     expect(response.body).toEqual(expectedBooks[1]);
-//   });
+  // // If a GET request is made to /api/cats/:id, does:
+  // // 1) the server respond with status of 200
+  // // 2) the body match the expected API data for the cat with that id?
+  // test('GET /api/books/:id', async () => {
+  //   const response = await request.get('/api/books/2');
+  //   expect(response.status).toBe(200);
+  //   expect(response.body).toEqual(expectedBooks[1]);
+  // });
 });
