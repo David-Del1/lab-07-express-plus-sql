b94d64b78bd7bbf380506ba0b29afe1528de8999
David Delgadillo <daviddel.731@gmail.com>
2021-05-11 15:20:18 -0700

    fixed pathing issue on images

--- data/books.js
+++ data/books.js
@@ -4,14 +4,14 @@ const books = [
     title: 'The Beginning of Infinity',
     author: 'David Deutsch',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/BoI.jpg',
+    image_url: '/assets/BoI.jpg',
     pub_year: 2011
   },
   {
     title: 'The Fabric of Reality',
     author: 'David Deutsch',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/FoR.jpg',
+    image_url: '/assets/FoR.jpg',
     pub_year: 1998
   },
   {
@@ -25,56 +25,56 @@ const books = [
     title: 'I Am A Strange Loop',
     author: 'Douglas Hofstadter',
     genre: 'Philosophy',
-    image_url: 'assets/IAASL.jpg',
+    image_url: '/assets/IAASL.jpg',
     pub_year: 2007
   },
   {
     title: 'The Ascent of Man',
     author: 'Jacob Bronowski',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/AoM.jpg',
+    image_url: '/assets/AoM.jpg',
     pub_year: 1973
   },
   {
     title: 'Enlightenment Now',
     author: 'Steven Pinker',
     genre: 'Social Science/Nonficiton',
-    image_url: 'assets/EN.webp',
+    image_url: '/assets/EN.webp',
     pub_year: 2018
   },
   {
     title: 'The Selfish Gene',
     author: 'Richard Dawkins',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/TSG.jpg',
+    image_url: '/assets/TSG.jpg',
     pub_year: 1976
   },
   {
     title: 'The Blind Watchmaker',
     author: 'Richard Dawkins',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/TBW.jpg',
+    image_url: '/assets/TBW.jpg',
     pub_year: 1986
   },
   {
     title: 'The Science of Can and Can\'t',
     author: 'Chiarla Marletto',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/SoCaC.jpg',
+    image_url: '/assets/SoCaC.jpg',
     pub_year: 2021
   },
   {
     title: 'Scale',
     author: 'Geoffrey West',
     genre: 'Science/Nonfiction',
-    image_url: 'assets/scale.jpg',
+    image_url: '/assets/scale.jpg',
     pub_year: 2017
   },
   {
     title: 'Homo Deus',
     author: 'Yuval Noah Harrari',
     genre: 'Social Science',
-    image_url: 'assets/HD.jpg',
+    image_url: '/assets/HD.jpg',
     pub_year: 2015
   }
 ];
